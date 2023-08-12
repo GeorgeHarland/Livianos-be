6bdb0f2d6c07519d4681f8708f1688a066188644
GeorgeH <georgeharland12@gmail.com>
2023-08-12 11:30:35 +0100

    converts message to string

--- main.go
+++ main.go
@@ -21,7 +21,8 @@ func reader(conn *websocket.Conn) {
 			log.Println(err)
 			return
 		}
-		log.Println(p)
+		log.Println("Bytes received: ", p)
+		log.Println("Message received: ", string(p))
 
 		if err := conn.WriteMessage(messageType, p); err != nil {
 			log.Println(err)
@@ -33,6 +34,10 @@ func reader(conn *websocket.Conn) {
 func handleRestRequest(w http.ResponseWriter, r *http.Request) {
 	log.Println("Rest endpoint reached")
 	var err error
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 	if r.Method == http.MethodGet {
 		json.NewEncoder(w).Encode("Thanks for the get request")
 	} else if r.Method == http.MethodPost {
